8598675715aa3ffb602cd98f8f42e6827de46bc8
Ian Moore <imoore@technophobia.com>
2012-10-18 12:12:20 +0100

    ammended gh-pages script

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -18,7 +18,7 @@ rm -rf _static
 
 # move content over from the _build dir
 
-cp docs/_build/html _static
+cp docs/_build/html/_static .
 cp docs/_build/html/*.html .
 cp docs/_build/html/*.inv .
 cp docs/_build/html/*.js .
